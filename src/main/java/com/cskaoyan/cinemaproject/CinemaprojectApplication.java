b717aa762f7e00d108036795418228cb045ef290
wsk <watermelon_1994@163.com>
2019-06-03 15:49:38 +0800

    第一次提交

---
+++ src/main/java/com/cskaoyan/cinemaproject/CinemaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.cskaoyan.cinemaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cskaoyan/cinemaproject/CinemaprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cskaoyan.cinemaproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CinemaprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
